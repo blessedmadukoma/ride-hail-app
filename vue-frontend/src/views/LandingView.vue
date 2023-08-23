4cbe26e5e9c38a4c560d049a395bfacaa8de88d5
Akpughe <davidakpughe2@gmail.com>
2023-08-23 23:59:06 +0100

    added driver standby and info filling page

--- vue-frontend/src/views/LandingView.vue
+++ vue-frontend/src/views/LandingView.vue
@@ -1,10 +1,25 @@
 <script setup>
 import { useRouter } from 'vue-router'
+import http from '../helpers/http';
 
 const router = useRouter()
 
 const handleFindARide = () => {
   router.push({ name: 'location' });
+}
+
+const handleStartDriving = () => {
+  http().get('/driver').then((response) => {
+    // if the authenticated user is a driver
+    if (response.data.driver) {
+      router.push({ name: 'standby' });
+    } else {
+      // else
+      router.push({ name: 'driver' });
+    }
+  }).catch((error) => {
+    console.log(error.response.data);
+  })
 }
 </script>
 
@@ -14,7 +29,7 @@ const handleFindARide = () => {
     <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
       <div class="bg-white px-4 py-5 sm:p-6">
         <div class="flex justify-between">
-          <button type="submit"
+          <button @click="handleStartDriving" type="submit"
             class="rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none">Start
             Driving</button>
           <button @click="handleFindARide" type="submit"
